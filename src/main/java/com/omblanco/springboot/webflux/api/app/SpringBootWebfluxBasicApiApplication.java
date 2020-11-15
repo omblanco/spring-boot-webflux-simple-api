1ea750e738062fd0e6389211925bba61df6e3d23
Oscar Martinez Blanco <omblanco@gmail.com>
2020-11-15 01:00:47 +0100

    Log aop para flux y mono fixed #6

--- src/main/java/com/omblanco/springboot/webflux/api/app/SpringBootWebfluxBasicApiApplication.java
+++ src/main/java/com/omblanco/springboot/webflux/api/app/SpringBootWebfluxBasicApiApplication.java
@@ -1,23 +1,18 @@
 package com.omblanco.springboot.webflux.api.app;
 
 import java.util.Date;
-import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import com.omblanco.springboot.webflux.api.app.services.UserService;
 import com.omblanco.springboot.webflux.api.app.web.dto.UserDTO;
 
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * Clase principal de configuración de Spring Boot
--- src/main/java/com/omblanco/springboot/webflux/api/app/web/controllers/UserController.java
+++ src/main/java/com/omblanco/springboot/webflux/api/app/web/controllers/UserController.java
@@ -15,9 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +23,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.WebExchangeBindException;
 
 import com.omblanco.springboot.webflux.api.app.services.UserService;
 import com.omblanco.springboot.webflux.api.app.web.dto.UserDTO;
