13c0a1560d52d3545d7d7c8318226e6773fb78cc
Oscar Martinez <omblanco@gmail.com>
2020-12-08 14:01:51 +0100

    Autenticación en el cliente fixed #40

---
+++ spring-boot-webflux-simple-api-client/src/main/java/com/omblanco/springboot/webflux/api/client/dto/LoginResponseDTO.java
@@ -0,0 +1,23 @@
+package com.omblanco.springboot.webflux.api.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Clase DTO con la respuesta del login con el token
+ * @author oscar.martinezblanco
+ *
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoginResponseDTO {
+
+    private String token;
+    
+}
